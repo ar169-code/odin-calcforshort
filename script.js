21897054e01aa45ca516d29fcc194df1c2098a9d
AN <arngu01@outlook.com>
2026-08-04 11:48:48 +0100

    Create functions to allow calculator to perform operations of two numbers
    Created functions that allow user to enter two numbers and an operator
    to perform a function. However, it only works for one equation at time.

--- script.js
+++ script.js
@@ -14,3 +14,85 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b
 }
+
+// Assign default variables
+let firstNumber = "";
+let currentOperator;
+let secondNumber = "";
+let beforeOperator = true;
+const operations = "+-*/";
+
+
+function operate(a, b, operator) {
+    let result
+
+    switch (operator) {
+        case "+":
+            result = add(a, b);
+            break
+        case "-":
+            result = subtract(a, b);
+            break
+        case "*":
+            result = multiply(a, b);
+            break
+        case "/":
+            result = divide(a, b);
+            break
+    }
+    
+    return result
+}
+
+const display = document.querySelector(".display")
+
+const buttons = document.querySelectorAll("button")
+
+for (let button of [...buttons].slice(0,-2)) {
+    button.addEventListener("click", () => {
+        const currentInput = button.textContent
+        if (!firstNumber) {
+            display.textContent = ""
+        }
+
+        display.textContent += currentInput;
+
+        if (beforeOperator) {
+            if (operations.includes(currentInput)) {
+                currentOperator = currentInput;
+                beforeOperator = false;
+                return;
+            }
+
+            firstNumber += currentInput;
+            return;
+        }
+
+        secondNumber += currentInput;
+
+
+    })
+}
+
+// Create functions for equals and clear that have different purposes
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+
+function resetCalc() {
+    firstNumber = "";
+    secondNumber = "";
+    currentOperator = null;
+    beforeOperator = true;
+}
+
+equals.addEventListener("click", () => {
+    const result = operate(+firstNumber, +secondNumber, currentOperator)
+
+    resetCalc()
+    display.textContent = result;
+})
+
+ clear.addEventListener("click",() => {
+    display.textContent = ""
+    resetCalc()
+ })
